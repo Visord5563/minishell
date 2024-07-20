dc6dd016c1b2e170766ab35e8247b8571316137d
mehdi <elmehdihafiane@gmail.com>
2024-07-20 18:06:46 +0100

    echo -n

--- bultins/echo.c
+++ bultins/echo.c
@@ -6,11 +6,12 @@
 /*   By: ehafiane <ehafiane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 10:15:41 by ehafiane          #+#    #+#             */
-/*   Updated: 2024/07/20 12:03:53 by ehafiane         ###   ########.fr       */
+/*   Updated: 2024/07/20 18:06:09 by ehafiane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 
 int count_argc(char **argv)
 {
@@ -19,33 +20,44 @@ int count_argc(char **argv)
 		i++;
 	return i;
 }
+int is_n_flag(char *arg)
+{
+	if (arg[0] == '-' && arg[1] == 'n')
+	{
+		for (int i = 2; arg[i] != '\0'; i++)
+		{
+			if (arg[i] != 'n')
+				return 0;
+		}
+		return 1;
+	}
+	return 0;
+}
 
 int echo(char **argv)
 {
 	int i = 1;
 	int flag = 0;
-	
+
 	if (count_argc(argv) > 1)
 	{
-		while (argv[i] && ft_strcmp(argv[i], "-n") == 0)
+		while (argv[i] && is_n_flag(argv[i]))
 		{
 			flag = 1;
 			i++;
 		}
-		while (argv[i] != NULL) 
+		while (argv[i] != NULL)
 		{
 			printf("%s", argv[i]);
-			if (argv[i+1] != NULL && argv[i][0] != '\0') {
+			if (argv[i+1] != NULL)
 				printf(" ");
-			}
 			i++;
 		}
 	}
 	if (flag == 0)
-		printf("\n");	
+		printf("\n");
 	return 1;
-	}
-
+}
 
 int main(int argc, char **argv)
 {
