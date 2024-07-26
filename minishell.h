8aba2f00427d19792d1aafd21aeb4c795d5e7c37
mehdi <elmehdihafiane@gmail.com>
2024-07-26 17:04:13 +0100

    awah

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ehafiane <ehafiane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:13:38 by saharchi          #+#    #+#             */
-/*   Updated: 2024/07/24 13:11:10 by ehafiane         ###   ########.fr       */
+/*   Updated: 2024/07/26 16:59:31 by ehafiane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,14 @@
 # include <signal.h>
 #include "./libft/libft.h"
 #include <fcntl.h>
+
+typedef struct g_signle
+{
+	int sig_herdoc;
+	int ma_in;
+} t_signle;
+
+t_signle g_sigl;
 
 typedef enum s_token
 {
@@ -81,18 +89,21 @@ t_flags g_flags;
 int		ft_echo(char **argv);
 void	ft_exit(char **str);
 int		ft_pwd();
-void ft_cd(char *arg, t_env **env);
+void	ft_cd(char *arg, t_env **env);
 void	real_env(t_env *a, int i, char **cmd);
 
 void	handle_redirection(t_data *data);
 void	execute_this(t_data *data);
 void	check_bultins(char **cmd, t_env **env);
-int if_bultins(char **cmd);
+int		if_bultins(char **cmd);
 
+int		count_str(char *str, char *set);
+char	**my_split(char *str, char *set);
 t_parse	*ft_lstnew(char *content, t_token token);
 void	ft_lstadd_back(t_parse **lst, t_parse *new);
 t_parse	*ft_lstlast(t_parse *lst);
 int		ft_lstsize(t_parse *lst);
 void	ft_lstclear(t_parse *lst);
+void	sighandel(int sig);
 
 #endif
---
+++ split.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehafiane <ehafiane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 11:06:06 by saharchi          #+#    #+#             */
+/*   Updated: 2024/07/26 16:58:41 by ehafiane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_space2(char c, char *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	count_str(char *str, char *set)
+{
+	int	i;
+	int	count;
+	char quote;
+
+	count = 0;
+	i = 0;
+	quote = '\0';
+	while (str[i] != '\0')
+	{
+		while (str[i] != '\0' && is_space2(str[i], set))
+			i++;
+		if (str[i] != '\0')
+			count++;
+		while (str[i] != '\0')
+		{
+			if (quote == '\0' && (str[i] == '"' || str[i] == '\''))
+				quote = str[i];
+			else if (str[i] == quote)
+				quote = '\0';
+			else if (quote == '\0' && ft_strchr(set, str[i]))
+				break;
+			i++;
+		}
+		// while (str[i] != '\0' && !is_space2(str[i], set) && str[i] != '"' && str[i] != '\'')
+		// 	i++;
+	}
+	return (count);
+}
+
+int	ft_strlen2(char *str, char *set)
+{
+	int	i;
+
+	i = 0;
+	char quote;
+	
+	quote = '\0';
+	// while (str[i] && !is_space2(str[i], set))
+	// 	i++;
+	while (str[i] != '\0')
+	{
+		if (quote == '\0' && (str[i] == '"' || str[i] == '\''))
+			quote = str[i];
+		else if (str[i] == quote)
+			quote = '\0';
+		else if (quote == '\0' && ft_strchr(set, str[i]))
+			break;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_word(char *str, char *set)
+{
+	int		len_word;
+	int		i;
+	char	*word;
+
+	i = 0;
+	len_word = ft_strlen2(str, set);
+	word = (char *)malloc(sizeof(char) * (len_word + 1));
+	while (i < len_word)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**my_split(char *str, char *set)
+{
+	char	**strings;
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = '\0';
+	strings = (char **)malloc(sizeof(char *)
+			* (count_str(str, set) + 1));
+	while (*str != '\0')
+	{
+		while (*str != '\0' && is_space2(*str, set))
+			str++;
+		if (*str != '\0')
+		{
+			strings[i] = ft_word(str, set);
+			i++;
+		}
+		// while (*str && !is_space2(*str, set))
+		// 	str++;
+		while (*str != '\0')
+		{
+			if (quote == '\0' && (*str == '"' || *str == '\''))
+				quote = *str;
+			else if (str[i] == quote)
+				quote = '\0';
+			else if (quote == '\0' && ft_strchr(set, *str))
+				break;
+			str++;
+		}
+	}
+	strings[i] = 0;
+	return (strings);
+}
