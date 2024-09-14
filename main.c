fad5531654290df80634a2dd2dcb00a84313e76a
mehdi <elmehdihafiane@gmail.com>
2024-09-14 10:45:34 +0100

    all norms checked done

--- main.c
+++ main.c
@@ -6,48 +6,49 @@
 /*   By: ehafiane <ehafiane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:13:31 by saharchi          #+#    #+#             */
-/*   Updated: 2024/09/14 10:37:35 by ehafiane         ###   ########.fr       */
+/*   Updated: 2024/09/14 10:44:54 by ehafiane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/minishell.h"
 
-int	main(int ac, char **av, char **env)
+void	init_data(t_data **data, char **env)
 {
-    char			*line;
-	t_data			*data;
-    t_parse			*parse;
-	struct termios	term;
-
-    (void)ac;
-    (void)av;
-	data = malloc(sizeof(t_data));
-	parse = NULL;
-	data->cmd = NULL;
-	data->env = NULL;
+	*data = malloc(sizeof(t_data));
+	(*data)->cmd = NULL;
+	(*data)->env = NULL;
 	if (!env[0])
 	{
-		data->flag = 1;	
-		ft_env(&data->env, env, 1);
+		(*data)->flag = 1;
+		ft_env(&(*data)->env, env, 1);
 	}
 	else
-		ft_env(&data->env, env, 0);
+		ft_env(&(*data)->env, env, 0);
+}
+
+void	setup_signals(void)
+{
 	rl_catch_signals = 0;
 	signal(SIGINT, sighandel);
 	signal(SIGQUIT, sighandel);
 	g_sigl.sig_child = 0;
-    while (1)
-    {
-		if (!ttyname(0))
-			break;
-        line = readline("\033[0;34mMinishell$ \033[0;37m");
-        if (!line)
-            break;
+}
+
+void	command_loop(t_data *data, t_parse *parse)
+{
+	char			*line;
+	struct termios	term;
+
+	while (ttyname(0))
+	{
+		line = readline("\033[0;34mMinishell$ \033[0;37m");
+		if (!line)
+			break ;
 		if (g_sigl.sig_int == SIGINT)
 			exit_status(&data->env, "1");
 		parsing(line, data, &parse);
 		if (line && *line)
-        	add_history(line);
+			add_history(line);
 		if (data->cmd)
 		{
 			tcgetattr(0, &term);
@@ -57,8 +58,21 @@ int	main(int ac, char **av, char **env)
 		ft_lstclearcmd(data->cmd);
 		data->cmd = NULL;
 		ft_lstclear(parse);
-        parse = NULL;
-        free(line);
+		parse = NULL;
+		free(line);
 	}
+}
+
+int	main(int ac, char **av, char **env)
+{
+	t_data	*data;
+	t_parse	*parse;
+
+	(void)ac;
+	(void)av;
+	parse = NULL;
+	init_data(&data, env);
+	setup_signals();
+	command_loop(data, parse);
 	return (0);
 }
